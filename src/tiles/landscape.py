3a0b11a73ab5edb10b9c10ef6fd0500c2b7f3bd9
João André Pestre <joaoandre.pestre@poli.ufrj.br>
2023-02-26 18:46:35 -0300

    attempt at creating build with no sound

--- src/tiles/landscape.py
+++ src/tiles/landscape.py
@@ -1,12 +1,12 @@
 from tiles.section import Section
-from ffpyplayer.player import MediaPlayer
+#from ffpyplayer.player import MediaPlayer
 
 
 class Landscape:
     def __init__(self, paths: list[str]) -> None:
         self.sections: list[Section] = [Section(path) for path in paths]
         self.audio_path = paths[0]
-        self.audio: MediaPlayer = None
+        self.audio = None
         self.playing_sections: int = 0
 
     def framestamp(self) -> int:
@@ -14,19 +14,21 @@ class Landscape:
 
     def start_audio(self) -> None:
         if (self.audio is None):
-            self.audio = MediaPlayer(self.audio_path)
+            pass
+            #self.audio = MediaPlayer(self.audio_path)
 
     def stop_audio(self) -> None:
         if (self.audio is not None):
             print('stopping audio')
-            self.audio.set_mute(True)
-            self.audio.close_player()
-            self.audio = None
+            # self.audio.set_mute(True)
+            # self.audio.close_player()
+            #self.audio = None
 
     def start_section(self, idx: int, resume: bool = False) -> Section:
         self.playing_sections += 1
         if (self.audio is None):
-            self.start_audio()
+            pass
+            # self.start_audio()
         if not resume:
             return self.sections[idx].start()
         else:
@@ -35,7 +37,8 @@ class Landscape:
     def stop_section(self, idx: int) -> None:
         self.playing_sections -= 1
         if (self.playing_sections == 0):
-            self.stop_audio()
+            pass
+            # self.stop_audio()
         return self.sections[idx].stop()
 
     def restart_section(self, idx: int) -> Section:
@@ -46,5 +49,6 @@ class Landscape:
         if (self.audio is not None):
             _, val = self.audio.get_frame(show=False)
             if val == 'eof':
-                self.stop_audio()
+                pass
+                # self.stop_audio()
         return self.sections[idx].read_frame()
---
+++ tyler.spec
@@ -0,0 +1,68 @@
+# -*- mode: python ; coding: utf-8 -*-
+import ffpyplayer
+FFPYPLAYER_PATH = ffpyplayer.__path__[0]
+
+block_cipher = None
+
+
+a = Analysis(
+    ['src\\main.py'],
+    pathex=['.', '.\\src', '.\\venv\\Lib\\site-packages'],
+    binaries=[
+        (f'{FFPYPLAYER_PATH}\\*.pyd', 'ffpyplayer'),
+        (f'{FFPYPLAYER_PATH}\\player\\*.pyd', 'ffpyplayer/player'),
+    ],
+    datas=[('statics', 'statics')],
+    hiddenimports=[
+        'ffpyplayer.threading',
+        'ffpyplayer.player.queue',
+        'ffpyplayer.player.frame_queue',
+        'ffpyplayer.player.decoder',
+        'ffpyplayer.player.clock',
+        'ffpyplayer.player.core',
+    ],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    win_no_prefer_redirects=False,
+    win_private_assemblies=False,
+    cipher=block_cipher,
+    noarchive=False,
+)
+pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    [],
+    exclude_binaries=True,
+    name='Tyler',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    console=False,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+)
+coll = COLLECT(
+    exe,
+    a.binaries,
+    a.zipfiles,
+    a.datas,
+    *[Tree(p) for p in ffpyplayer.dep_bins],
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    name='Tyler',
+)
+app = BUNDLE(
+    coll,
+    name='Tyler.app',
+    icon='statics\\tile-icon.ico',
+    bundle_identifier=None,
+)
